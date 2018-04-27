b7a53ddf36124c0043b16138464bf145d1b412f0
dat <taurandat@gmail.com>
2018-04-27 21:29:52 +0700

    Add "Remove Invalid Parentheses" problem

---
+++ algorithms/solutions/week19/taurandat/remove-invalid-parentheses.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int count = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') count++;
+            if(s[i] == ')') count--;
+            if(count < 0) return false;
+        }
+        return (count == 0);
+    }
+
+    vector<string> removeInvalidParentheses(string s) {
+        vector<string> answers;
+
+        set<string> visited;
+        visited.insert(s);
+
+        bool stopAcceptingMore = false;
+        queue<string> q;
+        q.push(s);
+        while(!q.empty()) {
+            s = q.front(); q.pop();
+            if(isValid(s)) {
+                answers.push_back(s);
+                stopAcceptingMore = true;
+            }
+
+            if(stopAcceptingMore) {
+                continue;
+            }
+
+            for(int i = 0; i < s.size(); i++) {
+                if(s[i] == '(' || s[i] == ')') {
+                    string temp = s.substr(0, i) + s.substr(i + 1);
+
+                    if (visited.find(temp) == visited.end()) {
+                        visited.insert(temp);
+                        q.push(temp);
+                    }
+                }
+            }
+        }
+
+        if (answers.size() == 0) {
+            answers.push_back("");
+        }
+
+        return answers;
+    }
+};
