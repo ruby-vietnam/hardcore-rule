bc23d0b645213787d10f31a9411643f56dd00939
dat <taurandat@gmail.com>
2018-04-29 01:49:01 +0700

    Dijkstra's trace back: MLE with 39/39

--- algorithms/solutions/week19/taurandat/word-ladder-ii.cpp
+++ algorithms/solutions/week19/taurandat/word-ladder-ii.cpp
@@ -1,11 +1,3 @@
-#include <vector>
-#include <string>
-#include <queue>
-#include <map>
-#include <set>
-
-using namespace std;
-
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord,
@@ -17,55 +9,74 @@ public:
         }
 
         // 1. Construct a graph
-        map<string, vector<pair<string, int>>> costsAdjacent;
-        if(find(wordList.begin(), wordList.end(), beginWord) == wordList.end()) {
-            wordList.push_back(beginWord);
+        int n = wordList.size() + 1;
+        vector<vector<int>> costs(n);
+        for(int i = 0; i < n; i++) {
+            costs[i].resize(n);
         }
-        for(auto &u: wordList) {
-            for(auto &v: wordList) {
-                if(u != v && isAdjacent(u, v)) {
-                    costsAdjacent[u].push_back(make_pair(v, 1));
+        for(int i = 0; i < wordList.size(); i++) {
+            if (isAdjacent(beginWord, wordList[i])) {
+                costs[0].push_back(i+1);
+                costs[i+1].push_back(0);
+            }
+        }
+        for(int i = 0; i < wordList.size(); i++) {
+            for(int j = i + 1; j < wordList.size(); j++) {
+                if(isAdjacent(wordList[i], wordList[j])) {
+                    costs[i+1].push_back(j+1);
+                    costs[j+1].push_back(i+1);
                 }
             }
         }
 
         // 2. Find the shortest path from beginWord to endWord
-        map<string, int> dist;
-        map<string, string> p;
-        for(auto& w: wordList) {
-            dist[w] = INT_MAX;
-        }   dist[beginWord] = 0;
+        vector<int> dist(n, INT_MAX);
+        dist[0] = 0;
+        vector<vector<int>> prev(n);
+        set<int> visited;
 
-        priority_queue <pair<int, string>> q;
-        q.push(make_pair(0, beginWord));
+        queue<int> q;
+        q.push(0);
         while (!q.empty()) {
-            string v = q.top().second;
-            int cur_d = -q.top().first;
-
+            int nodeFrom = q.front();
             q.pop();
-            if (cur_d > dist[v]) {
-                continue;
-            }
 
-            for (auto& edge: costsAdjacent[v]) {
-                string to = edge.first;
-                int len = edge.second;
-                if (dist[v] + len < dist[to]) {
-                    dist[to] = dist[v] + len;
-                    p[to] = v;
-                    q.push(make_pair(-dist[to], to));
+            for (auto& node: costs[nodeFrom]) {
+                int nodeTo = node;
+
+                if (dist[nodeFrom] + 1 < dist[nodeTo] && visited.find(nodeTo) == visited.end()) {
+                    dist[nodeTo] = dist[nodeFrom] + 1;
+                    prev[nodeTo].push_back(nodeFrom);
+                    visited.insert(nodeTo);
+                    q.push(nodeTo);
+                } else if (dist[nodeFrom] + 1 == dist[nodeTo]) {
+                    prev[nodeTo].push_back(nodeFrom);
                 }
             }
         }
 
         // 3. Generate all paths with given length from beginWord to endWord
-        int targetLength = dist[endWord];
+        int targetIndex = -1;
+        for(int i = 0; i < wordList.size(); i++) {
+            if(wordList[i] == endWord) {
+                targetIndex = i + 1;
+                break;
+            }
+        }
+        int targetLength = dist[targetIndex];
+        if (targetLength == INT_MAX) {
+            return answers;
+        }
 
-        /* DFS-based search */
-        answers = dfsFind(costsAdjacent, wordList, beginWord, endWord, targetLength);
+        /* DFS-based search: too slow */
+        // answers = dfsFind(costsAdjacent, wordList, beginWord, endWord, targetLength);
 
-        /* BFS-based search */
+        /* BFS-based search: too slow */
         // answers = bfsFind(costsAdjacent, beginWord, endWord, targetLength);
+
+        /* Dijkstra trace back: MLE */
+        vector<int> path;
+        trace(answers, wordList, beginWord, endWord, prev, path, targetIndex);
 
         return answers;
     }
@@ -89,6 +100,29 @@ private:
         return (count == 1);
     }
 
+    void trace(vector<vector<string>>& answers,
+               vector<string> wordList,
+               string beginWord,
+               string endWord,
+               vector<vector<int>> prev,
+               vector<int> path,
+               int current) {
+        if (current == 0) {
+            vector<string> answer;
+            answer.push_back(beginWord);
+            for(int i = path.size() - 1; i >= 0; i--) {
+                answer.push_back(wordList[path[i]]);
+            }
+            answers.push_back(answer);
+            path.clear();
+        } else {
+            path.push_back(current-1);
+            for (auto v: prev[current]) {
+                trace(answers, wordList, beginWord, endWord, prev, path, v);
+            }
+        }
+    }
+
     void dfsUtils(map<string, vector<pair<string, int>>>& costs,
                   string currentSource,
                   string target,
@@ -105,9 +139,8 @@ private:
         } else if (currentLength < maxLength) {
             for(auto& edge: costs[currentSource]) {
                 string newSource = edge.first;
-                int weight = edge.second;
-                if (visited.find(newSource) == visited.end() ) {
-                    dfsUtils(costs, newSource, target, visited, maxLength, currentLength+weight, paths, currentPath);
+                if (visited.find(newSource) == visited.end()) {
+                    dfsUtils(costs, newSource, target, visited, maxLength, currentLength+1, paths, currentPath);
                 }
             }
         }
