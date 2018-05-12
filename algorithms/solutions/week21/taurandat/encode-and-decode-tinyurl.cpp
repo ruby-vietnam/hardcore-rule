27a3ecce52b217a6f183efc23b0bc0f436354f80
Tat-Dat <taurandat@users.noreply.github.com>
2018-05-12 23:24:45 +0700

    solutions: Add Week21 solution (#393)
    * Add "Valid anagram" solution
    
    * Add counter example generator for lpvcpp's "Valid anagram" solution
    
    * Add "Encode and decode tinyurl" solution
    
    * Add "Merge k sorted lists" solution
    
    * Add "Serialize and Deserialize Binary Tree" solution
    
    * Add "Merge K sorted lists" solution with O(1) additional space
    
    * Add "Merge k sorted lists" solution with O(k) additional space; Clean up

---
+++ algorithms/solutions/week21/taurandat/encode-and-decode-tinyurl.cpp
@@ -0,0 +1,21 @@
+/*
+ *
+ * Link: https://leetcode.com/problems/encode-and-decode-tinyurl/
+ *
+ * 739 / 739 test cases passed.
+ * Status: Accepted
+ * Runtime: 6 ms
+ * Your runtime beats 99.75 % of cpp submissions.
+ *
+ */
+
+class Solution {
+public:
+    string encode(string longUrl) {
+        return longUrl;
+    }
+
+    string decode(string shortUrl) {
+        return shortUrl;
+    }
+};
