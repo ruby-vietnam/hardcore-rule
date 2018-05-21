22c4c01870b378b859954ee3541ff2000930f5ab
dat <taurandat@gmail.com>
2018-05-21 23:51:14 +0700

    Add week23's solutions

---
+++ algorithms/solutions/week23/taurandat/intersection-of-two-linked-lists.cpp
@@ -0,0 +1,63 @@
+/*
+ *
+ * Link: https://leetcode.com/problems/intersection-of-two-linked-lists
+ *
+ * 42 / 42 test cases passed.
+ * Status: Accepted
+ * Runtime: 35 ms
+ * Your runtime beats 98.66 % of cpp submissions.
+ *
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *mergePoint = nullptr;
+
+        int lengthA = getLength(headA);
+        int lengthB = getLength(headB);
+
+        if (lengthA < lengthB) {
+            swap(lengthA, lengthB);
+            swap(headA, headB);
+        }
+
+        int diff = lengthA - lengthB;
+        while(diff--) {
+            headA = headA->next;
+        }
+
+        while(headA != nullptr && headB != nullptr) {
+            if(headA->val == headB->val) {
+                mergePoint = headA;
+                break;
+            }
+
+            headA = headA->next;
+            headB = headB->next;
+        }
+
+        return mergePoint;
+    }
+
+private:
+    int getLength(ListNode *p) {
+        int length = 0;
+
+        ListNode *clone = p;
+        while(clone != nullptr) {
+            length++;
+            clone = clone->next;
+        }
+
+        return length;
+    }
+};
