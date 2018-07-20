ef431f56699e386e6c15da9904e06825ac4ba092
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-07-20 11:05:50 +0200

    Add some comments and enhance testing

--- algorithms/solutions/week31/qcam/abba.exs
+++ algorithms/solutions/week31/qcam/abba.exs
@@ -3,18 +3,22 @@ defmodule Week31.ABBA do
     detect(string, {false, false})
   end
 
+  # When the current string is empty, finish parsing.
   defp detect(<<>>, {has_ab?, has_ba?}) do
     has_ab? and has_ba?
   end
 
+  # When the current string starts with "AB".
   defp detect(<<"AB", rest::bits>>, {_has_ab?, has_ba?}) do
     detect(rest, {true, has_ba?})
   end
 
+  # When the current string starts with "BA".
   defp detect(<<"BA", rest::bits>>, {has_ab?, _has_ba?}) do
     detect(rest, {has_ab?, true})
   end
 
+  # When the first character of the current string does not match any clause above.
   defp detect(<<_::integer, rest::bits>>, flags) do
     detect(rest, flags)
   end
@@ -30,8 +34,10 @@ defmodule Week31.ABBATest do
   test "detect/1" do
     assert detect("ABBBBA")
     assert detect("ABABBBAB")
+    assert detect("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBA")
     refute detect("ABA")
     refute detect("AXBYBXA")
     refute detect("")
+    refute detect("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
   end
 end
