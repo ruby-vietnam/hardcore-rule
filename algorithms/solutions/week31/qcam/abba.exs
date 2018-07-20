2079057bf916e8a2b0d6239349e2b94ac9077253
Cẩm Huỳnh <huynhquancam@gmail.com>
2018-07-20 12:14:48 +0200

    Cover edge cases in ABBA solution
    Kudos to `@thaod` for contributing test cases.

--- algorithms/solutions/week31/qcam/abba.exs
+++ algorithms/solutions/week31/qcam/abba.exs
@@ -1,11 +1,25 @@
 defmodule Week31.ABBA do
   def detect(string) do
-    detect(string, {false, false})
+    {has_ab?, has_ba?} = detect(string, {false, false})
+
+    has_ab? and has_ba?
   end
 
   # When the current string is empty, finish parsing.
   defp detect(<<>>, {has_ab?, has_ba?}) do
-    has_ab? and has_ba?
+    {has_ab?, has_ba?}
+  end
+
+  # Stop parsing when we see both "AB" and "BA" appear.
+  defp detect(_string, {true, true}) do
+    {true, true}
+  end
+
+  defp detect(<<lookaheads::binary-size(3), rest::bits>>, {has_ab?, has_ba?})
+       when lookaheads in ["ABA", "BAB"] do
+    {will_have_ab?, will_have_ba?} = detect(rest, {has_ab?, has_ba?})
+
+    {true, will_have_ab? or will_have_ba?}
   end
 
   # When the current string starts with "AB".
@@ -39,5 +53,8 @@ defmodule Week31.ABBATest do
     refute detect("AXBYBXA")
     refute detect("")
     refute detect("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+    assert detect("ABAXXAB")
+    assert detect("BABAB")
+    refute detect("BABA")
   end
 end
